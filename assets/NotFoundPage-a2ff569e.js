c1ac5b9275929bfb794fea9e466f1ee79a2e2254
Abdullah Tayran <abdullahtayran999@gmail.com>
2025-08-08 00:47:34 +0300

    Updates

---
+++ assets/NotFoundPage-a2ff569e.js
@@ -0,0 +1,1 @@
+import{j as o}from"./vendor-3d852f05.js";import{P as r}from"./ProdErrorPage-f641470f.js";function a(){return o.jsx(r,{text:"Page not found.",canRefresh:!1})}export{a as default};
---
+++ assets/SomethingWentWrongPage-16ad85ab.js
@@ -0,0 +1,1 @@
+import{j as r}from"./vendor-3d852f05.js";import{P as t}from"./ProdErrorPage-f641470f.js";function n(){return r.jsx(t,{text:"Something went wrong.",canRefresh:!0})}export{n as default};
